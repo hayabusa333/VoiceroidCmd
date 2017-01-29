f48cf436decf4e9e38ec7f5a28e27d859acfe309
Junnichi Kitta <soiru_megane12@hotmail.com>
2017-01-29 14:56:50 +0900

    Japanese correspondence

--- VoiceroidCmd/Main.cpp
+++ VoiceroidCmd/Main.cpp
@@ -9,23 +9,23 @@ BOOL CALLBACK SearchVoiceroid(HWND hwnd, LPARAM lp);
 HWND CALLBACK SearchTextArea(HWND mainWindow);
 void sendText(HWND hwnd, std::string text);
 HWND CALLBACK SearchPlayButton(HWND mainWindow);
+std::string wstring2string(const std::wstring &src);
 
 // オプション用の構造体
 struct Options {
 	// 読み上げ用文字列
-	std::string comment;
+	std::wstring comment;
 } typedef Options;
 
 HWND voiceroid;
 
-void main(int argc, char* argv[]) {
+void _tmain(int argc, _TCHAR* argv[]) {
 	Options option;
 
 	namespace po = boost::program_options;
 	po::options_description opt("オプション");
 	opt.add_options()
-		("help,h", "ヘルプを表示")
-		("comment,c", po::value<std::string>(), "コメント");
+		("help,h", "ヘルプを表示");
 	po::variables_map vm;
 	try
 	{
@@ -41,18 +41,16 @@ void main(int argc, char* argv[]) {
 	{
 		std::cout << opt << std::endl;
 	}
-	else
-	{
-		try
-		{
-			option.comment = vm["comment"].as<std::string>();
-			std::cout << option.comment << std::endl;
-		}
-		catch (const boost::bad_any_cast& e)
-		{
-			std::cout << e.what() << std::endl;
-		}
+
+	auto pr = parse_command_line(argc, argv, opt);
+	std::wstring echo_text = _T("");
+
+	// オプション以外のコマンドライン引数取得
+	for (auto const& str : collect_unrecognized(pr.options, po::include_positional)) {
+		echo_text.append(str);
 	}
+
+	printf("echo_text:%d\n", echo_text.length());
 
 	// Voiceroidのハンドラの取得
 	EnumWindows(SearchVoiceroid, 0x0);
@@ -61,7 +59,7 @@ void main(int argc, char* argv[]) {
 
 	SendMessage(textArea, WM_SETTEXT, (WPARAM)_T(""), NULL);
 
-	sendText(textArea, option.comment);
+	sendText(textArea, wstring2string(echo_text));
 
 	HWND play_button = SearchPlayButton(voiceroid);
 	SendMessage(play_button, BM_CLICK, 0, 0);
@@ -103,6 +101,8 @@ void sendText(HWND hwnd, std::string text) {
 	wchar_t *wcs = new wchar_t[text.length() + 1];
 	errno_t err = mbstowcs_s(&num, wcs, text.length() + 1, text.c_str(), _TRUNCATE);
 
+	printf("wcslen(wcs):%d\n", wcslen(wcs));
+
 	for (int i = 0; i < wcslen(wcs); i++) {
 		SendMessage(hwnd, WM_CHAR, (WPARAM)wcs[i], 0);
 	}
@@ -123,4 +123,19 @@ HWND CALLBACK SearchPlayButton(HWND mainWindow) {
 	
 	return hwnd;
 	
+}
+
+std::string wstring2string(const std::wstring &src) {
+	//ロケール指定
+	setlocale(LC_ALL, "japanese");
+
+	size_t num = 0;
+
+	char *mbs = new char[src.length() * MB_CUR_MAX + 1];
+	errno_t err = wcstombs_s(&num, mbs, src.length() * MB_CUR_MAX + 1, src.c_str(), _TRUNCATE);
+
+	std::string dest = mbs;
+	delete[] mbs;
+
+	return dest;
 }
